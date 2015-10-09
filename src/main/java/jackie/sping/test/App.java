90d5585457bc2c8c9fa6f754a78664e1356cca2a
jackiechan618 <jackiechan618@hotmail.com>
2015-10-08 23:19:29 -0400

    123

--- src/main/java/jackie/sping/test/App.java
+++ src/main/java/jackie/sping/test/App.java
@@ -4,10 +4,10 @@ package jackie.sping.test;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
+
+
